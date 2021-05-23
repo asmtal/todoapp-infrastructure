3cdbad5f1e57642aad95be04191a7532c75c9f1c
Joel Freeman <joel@joelfreeman.xyz>
2021-05-23 17:28:23 +1200

    commetned out stuff

--- main.tf
+++ main.tf
@@ -59,22 +59,22 @@ module "billing-alert" {
   }
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+// module "vpc" {
+//   source = "terraform-aws-modules/vpc/aws"
 
-  name = "todo-app"
-  cidr = "10.0.0.0/16"
+//   name = "todo-app"
+//   cidr = "10.0.0.0/16"
 
-  azs                = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
-  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24"]
-  enable_nat_gateway = true
+//   azs                = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
+//   private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+//   public_subnets     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24"]
+//   enable_nat_gateway = true
 
-  tags = {
-    Terraform   = "true"
-    Environment = "Development"
-  }
-}
+//   tags = {
+//     Terraform   = "true"
+//     Environment = "Development"
+//   }
+// }
 
 // module "vpn" {
 //   source = "./modules/vpn"
