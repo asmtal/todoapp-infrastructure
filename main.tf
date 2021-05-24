ab7063fe54098f5f77bbce60e8d6faf9e51628d0
Joel Freeman <joel@joelfreeman.xyz>
2021-05-24 13:28:58 +1200

    Added correct accounts

--- main.tf
+++ main.tf
@@ -35,6 +35,57 @@ provider "aws" {
 }
 
 
+// resource "aws_organizations_account" "prod" {
+//   name      = "jfreeman-dev"
+//   email     = "joel+aws-prod@jxel.dev"
+// }
+
+// resource "aws_organizations_account" "dev" {
+//   name      = "jfreeman-dev"
+//   email     = "joel+aws-dev@jxel.dev"
+// }
+
+resource "aws_organizations_organization" "org" {
+
+  aws_service_access_principals = [
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com"
+  ]
+
+  feature_set = "ALL"
+}
+
+// TODO:
+// Add GuardDuty
+// Add Config
+// Add SecurityHb
+// Add CloudTrail
+// Add roles in each Account
+
+resource "aws_organizations_account" "security" {
+  name      = var.security_account_name
+  email     = var.security_account_email
+  role_name = "Admin"
+
+resource "aws_organizations_account" "logs" {
+  name      = var.logs_account_name
+  email     = var.logs_account_email
+  role_name = "Admin"
+}
+
+resource "aws_organizations_account" "dev" {
+  name      = var.dev_account_name
+  email     = var.dev_account_email
+  role_name = "Admin"
+}
+
+resource "aws_organizations_account" "prod" {
+  name      = var.prod_account_name
+  email     = var.prod_account_email
+  role_name = "Admin"
+}
+
+
 module "iam" {
   source     = "./modules/iam"
   account_id = var.aws_account_id
@@ -58,3 +109,41 @@ module "billing-alert" {
     aws = aws.us-east-1
   }
 }
+
+// module "iam_account_prod" {
+//   source  = "terraform-aws-modules/iam/aws//modules/iam-account"
+//   version = "~> 3.0"
+
+//   account_alias = "jfreeman-prod"
+
+//   minimum_password_length = 37
+//   require_numbers         = false
+// }
+
+// module "iam_account_dev" {
+//   source  = "terraform-aws-modules/iam/aws//modules/iam-account"
+//   version = "~> 3.0"
+
+//   account_alias = "jfreeman-dev"
+
+//   minimum_password_length = 37
+//   require_numbers         = false
+// }
+
+
+// module "iam_assumable_roles" {
+//   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-roles"
+//   version = "~> 3.0"
+
+//   trusted_role_arns = [
+//     "arn:aws:iam::${module.iam_account_dev.}:root",
+//   ]
+
+//   create_admin_role = true
+
+//   create_poweruser_role = true
+//   poweruser_role_name   = "developer"
+
+//   create_readonly_role       = true
+//   readonly_role_requires_mfa = false
+// }
